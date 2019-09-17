fd01b7606405d18c858616e5881e72a36f7c5752
Richard <richardli125@gmail.com>
2019-09-16 23:25:47 -0400

    updated app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <main style={{marginTop: '64px'}}>
+        <p>This is the page content</p>
+      </main>
       
     </div>
   );
