83f7ac6893715ea4ea7a053f3811d50e4f694855
Richard <richardli125@gmail.com>
2019-09-17 23:13:49 -0400

    added png icons and are using those instead of font awesome because the free ones don't that good

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./header.styles.scss";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import deleteIcon from '../../assets/icons/icons8-delete-100.png';
+import menuIcon from '../../assets/icons/icons8-menu-96.png';
 
 const Header = props => (
   <header className='header'>
@@ -17,6 +22,15 @@ const Header = props => (
             <Link to='/about'>About</Link>
           </li>
         </ul>
+      </div>
+      <div className='menu-icon'>
+        {props.menuClicked ? (
+          // <FontAwesomeIcon onClick={props.handleMenuClicked} icon={faBars} size='2x' />
+          <img onClick={props.handleMenuClicked} style={{width: '25px'}} src={deleteIcon} alt="delete"/>
+        ) : (
+          // <FontAwesomeIcon onClick={props.handleMenuClicked} icon={faTimes} size='lg' />
+          <img onClick={props.handleMenuClicked} style={{width: '25px'}} src={menuIcon} alt="menu"/>
+        )}
       </div>
     </nav>
   </header>
