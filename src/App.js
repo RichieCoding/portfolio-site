d5eb8da4a449dad626dbc3710fa322fdf7dee68b
Richard <richardli125@gmail.com>
2019-09-20 05:15:17 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import MenuPage from "./components/MenuPage/MenuPage";
 import { Switch, Route } from "react-router-dom";
+import ProjectPage from "./Pages/Projects Page/ProjectPage";
 
 class App extends Component {
   state = {
@@ -23,10 +24,13 @@ class App extends Component {
           menuClicked={this.state.menuClicked}
         />
         <Switch>
+        { this.state.menuClicked ? <MenuPage handleMenuClicked={this.handleMenuClicked} /> : null}
+        <Route exact path='/projects' component={ProjectPage} />
           <div className='App'>
-            { this.state.menuClicked ? <MenuPage /> : null}
-            {/* <MenuPage /> */}
-          </div>
+            
+          </div> 
+          
+          
         </Switch>
       </>
     );
