2f3531180649c90d65e6e523831629860f461cd8
Richard <richardli125@gmail.com>
2019-09-21 02:46:59 -0400

    upaded project tile to have hover text

--- src/components/Project Tile/ProjectTile.js
+++ src/components/Project Tile/ProjectTile.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import './project-tile.styles.scss'
 
 const ProjectTile = ({image, title, description}) => {
   return (
     <div className='project-tile'>
       <h3 className='project-title'>{title}</h3>
       <img className='project-image' src={image} alt="project" />
+      <p>{description}</p>
+      <div className='top'>
+        <h2 className='hover-title'>{title}</h2>
+      </div>
     </div>
   )
 }
