c575c784edf67ebb9b0dc69dc133943aa7fc0d23
Richard <richardli125@gmail.com>
2019-09-22 16:32:51 -0400

    removed import for app css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import "./App.css";
 import Header from "./components/Header/Header";
 import MenuPage from "./components/MenuPage/MenuPage";
 import { Switch, Route } from "react-router-dom";
 import ProjectPage from "./Pages/Projects Page/ProjectPage";
+import ShoppiePage from "./Pages/Shoppie-Page/ShoppiePage";
 
 class App extends Component {
   state = {
@@ -25,11 +25,9 @@ class App extends Component {
         />
         <Switch>
         { this.state.menuClicked ? <MenuPage handleMenuClicked={this.handleMenuClicked} /> : null}
-        <Route exact path='/projects' component={ProjectPage} />
-          <div className='App'>
-            
-          </div> 
-          
+        <Route exact path ='/projects' component={ProjectPage} />
+        <Route exact path ='/' component={ProjectPage} />
+        <Route exact path ='/projects/Shoppie' component={ShoppiePage} />
           
         </Switch>
       </>
