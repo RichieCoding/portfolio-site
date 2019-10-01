2b8dc5cd4116edeba414404ed30318e9f3f9e83f
Richard Li <richardli125@gmail.com>
2019-10-01 02:19:20 -0400

    moved MenuPage to Page folder

---
+++ src/Pages/MenuPage/MenuPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './menu-page.styles.scss'
+import { Link } from 'react-router-dom'
+
+const MenuPage = (props) => {
+  return (
+    <div className='menu-page'>
+      <Link to='/projects' onClick={props.handleMenuClicked}><h3>Work</h3></Link>
+      <Link to='/about' onClick={props.handleMenuClicked}><h3>About</h3></Link>
+      <Link to='/contact' onClick={props.handleMenuClicked}><h3>Contact</h3></Link>
+    </div>
+  )
+}
+
+export default MenuPage
