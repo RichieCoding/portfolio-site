442969ae66ea1c1bb26a560a5cf84712801d9d92
Richard <richardli125@gmail.com>
2019-10-06 19:19:31 -0400

    updated project page

--- src/Pages/Projects Page/ProjectPage.js
+++ src/Pages/Projects Page/ProjectPage.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import './project-page.styles.scss'
-import Shoppie from '../../assets/images/shoppie-tile.png'
-import Chello from '../../assets/images/chello-tile.png'
-import Critter from '../../assets/images/Critter-02.png'
-import Shoppie4 from '../../assets/images/oKupid.png'
-import ProjectTile from '../../components/Project Tile/ProjectTile'
+import React from "react";
+import "./project-page.styles.scss";
+import Shoppie from "../../assets/images/shoppie-tile.png";
+import Chello from "../../assets/images/chello-tile.png";
+import Critter from "../../assets/images/Critter-02.png";
+import Shoppie4 from "../../assets/images/oKupid.png";
+import ProjectTile from "../../components/Project Tile/ProjectTile";
 
 const ProjectPage = () => {
   return (
     <div className='project-page'>
-      <ProjectTile image={Shoppie} title='Shoppie' />
+      <ProjectTile image={Shoppie} title='Shoppie' description="Shopify Clone" />
       <ProjectTile image={Chello} title='Chello' />
       <ProjectTile image={Critter} title='Critter' />
       <ProjectTile image={Shoppie4} title='oKupid' />
     </div>
-  )
-}
+  );
+};
 
-export default ProjectPage
+export default ProjectPage;
