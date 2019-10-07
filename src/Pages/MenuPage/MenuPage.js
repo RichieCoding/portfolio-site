3b4d06931e11f17c3178cc80b6638a1e5b66d38b
Richard <richardli125@gmail.com>
2019-10-06 21:09:37 -0400

    adjusted titles

--- src/Pages/MenuPage/MenuPage.js
+++ src/Pages/MenuPage/MenuPage.js
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom'
 const MenuPage = (props) => {
   return (
     <div className='menu-page'>
-      <Link to='/projects' onClick={props.handleMenuClicked}><h3>Projects</h3></Link>
-      <Link to='/about' onClick={props.handleMenuClicked}><h3>About</h3></Link>
-      <Link to='/contact' onClick={props.handleMenuClicked}><h3>Contact</h3></Link>
+      <Link to='/about' onClick={props.menuClicked}><h3>About</h3></Link>
+      <Link to='/projects' onClick={props.menuClicked}><h3>Projects</h3></Link>
+      <Link to='/contact' onClick={props.menuClicked}><h3>Contact</h3></Link>
     </div>
   )
 }
