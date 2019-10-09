579fb5dc1829be2c999d1b6d74d8fdf00492ecfb
Richard <richardli125@gmail.com>
2019-10-09 15:12:47 -0400

    added text for smaller height

--- src/Pages/About-Page/AboutPage.js
+++ src/Pages/About-Page/AboutPage.js
@@ -1,32 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import "./about-page.styles.scss";
 
-const AboutPage = () => {
-  return (
-    <div className='about-page'>
-      <div className='main-purple-box'></div>
-      <div className='text'>
-        <h2 className='text-name-intro'>Hi, I'm Richard</h2>
-        <div className='header1'>
-          <p className='fullstack'>a</p>
-          <p className='fullstack'>full</p>
-          <p className='fullstack'>stack</p>
-          <p className='fullstack'>developer</p>
+class AboutPage extends Component {
+  handleScroll = e => {
+    // const bottom = e.target.scrollHeight;
+    console.log("hello");
+  };
+
+  render() {
+    return (
+      <div className='about-page' onScroll={this.handleScroll}>
+        <div className='main-purple-box'></div>
+        <div className='text'>
+          <h2 className='text-name-intro'>Hi, I'm Richard</h2>
+          <div className='header1'>
+            <p className='fullstack'>a</p>
+            <p className='fullstack'>full</p>
+            <p className='fullstack'>stack</p>
+            <p className='fullstack'>developer</p>
+          </div>
+          <div className='fullstack-alt-height'>
+            <p>a full stack developer</p>
+          </div>
+        </div>
+        <div className='text2'>
+          <p className='header2'>
+            I have experience with JavaScript, React/Redux, HTML/CSS, Ruby on
+            Rails. I discovered Software Development through a friend that had
+            recommended it to me because of my love for Brazilian Jiu Jitsu and
+            how similar they are with problem solving and learning. I gave it
+            and a try and I absolutely fell in love with coding. I also enjoy
+            meeting and conversing with people so feel free to connect and chat.
+          </p>
         </div>
       </div>
-      <div className='text2'>
-        <p className='header2'>
-          I have experience with JavaScript,
-          React/Redux, HTML/CSS, Ruby on Rails. I discovered Software
-          Development through a friend that had recommended it to me because of
-          my love for Brazilian Jiu Jitsu and how similar they are with problem
-          solving and learning. I gave it and a try and I absolutely fell in
-          love with coding. I also enjoy meeting and conversing with people so
-          feel free to connect and chat.
-        </p>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default AboutPage;
