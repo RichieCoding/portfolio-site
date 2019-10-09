b4786e44a1e02bfd6e975dfc1d23013cc207e922
Richard <richardli125@gmail.com>
2019-10-09 15:15:01 -0400

    added security issue with target attribute

--- src/Pages/Shoppie-Page/ShoppiePage.js
+++ src/Pages/Shoppie-Page/ShoppiePage.js
@@ -1,34 +1,50 @@
-import React from 'react'
-import './shoppie-page.styles.scss'
-import Shoppie from '../../assets/images/main-shoppie.png'
-import Timeline from '../../components/Timeline/Timeline'
-import ScrollAnimation from 'react-animate-on-scroll';
+import React from "react";
+import "./shoppie-page.styles.scss";
+import Shoppie from "../../assets/images/main-shoppie.png";
+import Timeline from "../../components/Timeline/Timeline";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const ShoppiePage = () => {
   return (
     <div className='shoppie-page'>
       <div className='main-image'>
         <div className='image-container'>
-          <img src={Shoppie} alt="project"/>
+          <img src={Shoppie} alt='project' />
         </div>
         <div className='project-buttons'>
-          <a href="https://github.com/RichieCoding/Ecommerce-Front-End" target='_blank'><p>Demo</p></a>
-          <a href="https://github.com/RichieCoding/Ecommerce-Front-End" target='_blank'><p>Github</p></a>
+          <a
+            href='https://github.com/RichieCoding/Ecommerce-Front-End'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <p>Demo</p>
+          </a>
+          <a
+            href='https://github.com/RichieCoding/Ecommerce-Front-End'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <p>Github</p>
+          </a>
         </div>
       </div>
       <div className='shoppie-header'>
         <div className='main-header'>
           <h3>Shoppie</h3>
-          <p>An ecommerce site where an admin can sign in and manage their ecommerce site. They can track, add, and edit their inventory and view orders
-  that were created by customers. A customer can view products, log in and view orders they've placed.</p>
+          <p>
+            An ecommerce site where an admin can sign in and manage their
+            ecommerce site. They can track, add, and edit their inventory and
+            view orders that were created by customers. A customer can view
+            products, log in and view orders they've placed.
+          </p>
         </div>
         <div className='credentials'>
-          <div style={{margin: '20px 0'}}>
+          <div style={{ margin: "20px 0" }}>
             <h4 className='side-title'>Admin</h4>
             <p>Username: richie</p>
             <p>Password: abc123</p>
           </div>
-          <div style={{margin: '20px 0'}}>
+          <div style={{ margin: "20px 0" }}>
             <h4 className='side-title'>Customer</h4>
             <p>Username: test</p>
             <p>Password: test</p>
@@ -37,11 +53,11 @@ const ShoppiePage = () => {
       </div>
       <hr></hr>
       <h2 className='timeline-title'>TechStack</h2>
-      <ScrollAnimation animateIn="fadeIn">
+      <ScrollAnimation animateIn='fadeIn'>
         <Timeline />
       </ScrollAnimation>
     </div>
-  )
-}
+  );
+};
 
-export default ShoppiePage
+export default ShoppiePage;
