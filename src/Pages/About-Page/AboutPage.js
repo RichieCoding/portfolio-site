f998d712d6ecaaff59ea6d4a7cdecf076da45276
Richard <richardli125@gmail.com>
2019-10-10 09:22:03 -0400

    added fade in animation

--- src/Pages/About-Page/AboutPage.js
+++ src/Pages/About-Page/AboutPage.js
@@ -71,9 +71,7 @@ class AboutPage extends Component {
         <div className='text2'>
           <p className='header2'>
             I have experience with JavaScript, React/Redux, HTML/CSS, Ruby on
-            Rails. I discovered Software Development through a friend that had
-            recommended it to me because of my love for Brazilian Jiu Jitsu and
-            how similar they are with problem solving and learning. I gave it
+            Rails. I discovered Software Development through my love for Brazilian Jiu Jitsu and how similar they are with problem solving and learning. I gave it
             and a try and I absolutely fell in love with coding. I also enjoy
             meeting and conversing with people so feel free to connect and chat.
           </p>
